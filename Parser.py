5eb282afeff6a7c77e382ea10c68c8870f8064ed
goodship1 <msgoodship1@gmail.com>
2022-03-04 18:10:17 +0000

    working on parser

--- Parser.py
+++ Parser.py
@@ -2,12 +2,44 @@ import ply.lex as lex
 import ply.yacc as yacc
 from Lexer import tokens
 
-class Parser():
+symboltable = {}
+
+def p_assignment(p):
+    'assignment : VAR whitespace identifier whitespace equals whitespace expression colon'
+    p[0] = ('assignment',p[3],p[5],p[7])
+
+def p_expression_plus(p):
+	'add : expression  plus  expression colon'
+	p[0] = ('add' ,p[1],p[3],p[5])
+	
+	
+
+def p_integer(p):
+    'expression : integer'
+    p[0] = p[1]
 
 
-    def build(self):
-		parser = yacc.yacc()
+def p_float(p):
+    'expression : float'
+    p[0] = p[1]
+
+def p_string(p):
+    'expression : string'
+    p[0] = p[1]
+
+def p_plus(p):
+    'expression : plus'
+    p[0] = p[1]
 
 
-p = Parser()
-p.build()
+parser = yacc.yacc()
+while True:
+    try:
+        s = raw_input('Lox > ')
+    except EOFError:
+        break
+    if not s: continue
+    result = parser.parse(s)
+    print(result)
+
+print(symboltable)
