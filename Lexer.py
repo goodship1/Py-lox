5a1bf7d2ee3247eb97e0b4f6d649abee46894810
goodship1 <msgoodship1@gmail.com>
2022-03-04 13:20:44 +0000

    lexer rewrite

--- Lexer.py
+++ Lexer.py
@@ -1,179 +1,164 @@
 import ply.lex  as scanner
-class Lexer(object):
     #lexer for the lox programming language
-    key_words = {"if":"IF","else":"ELSE","print":"PRINT","true":"TRUE",
+key_words = {"if":"IF","else":"ELSE","print":"PRINT","true":"TRUE",
             "false":"FALSE","nil":"NIL","class":"CLASS","or":"OR","and":"AND"
             ,"var":"VAR","for":"FOR","fun":"FUN","while":"WHILE","return":"RETURN","init":"INIT","this":"THIS"}
 
-    token = ["plus","minus","equals","colon","times","leftpara","rightpara",
+tokens = ["plus","minus","equals","colon","times","leftpara","rightpara",
             "leftclosure","rightclosure","integer","float","string","greaterthan","lessthan"
             ,"identifier","whitespace","lessthanequal","greaterthanequal","float","increment","equalequal","constructor"]+list(key_words.values())
-    
+   
 
 
-    def t_indentifier(self,t):
+def t_indentifier(t):
         #indentifiers follow the following grammar var id = type; eg var name = "lox"
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.key_words.get(t.value,'identifier')
+        t.type = key_words.get(t.value,'identifier')
         return  t
 
-    def t_float(self,t):
+def t_float(t):
 		r'\d+\.\d+'
 		'[-+]?[0-9]+(\.([0-9]+)?([eE][-+]?[0-9]+)?|[eE][-+]?[0-9]+)'        
 		t.value = float(t.value)
 		return t
     
-    def t_nil(self,t):
+def t_nil(t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.key_words.get(t.value,"nil")
+        t.type = key_words.get(t.value,"nil")
         return t
 
-    def t_init(self,t):
+def t_init(t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.key_words.get("init","constructor")
+        t.type = key_words.get("init","constructor")
         return t
 
     
-    def t_plus(self,t):
+def t_plus(t):
         r'\+'
-        t.type = self.key_words.get(t.value,"plus")
+        t.type = key_words.get(t.value,"plus")
         return t
     
-    def t_else(self,t):
+def t_else(t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.key_words.get(t.value,"else")
+        t.type = key_words.get(t.value,"else")
         return t
 
 
-    def t_string(self,t):
+def t_string(t):
         r'"([^"\n]|(\\"))*"'
-        t.type = self.key_words.get(t.value,"string")
+        t.type = key_words.get(t.value,"string")
         return t
     
-    def t_while(self,t):
+def t_while(t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.key_words.get(t.value,"while")
+        t.type = key_words.get(t.value,"while")
         return t
 
-    def t_for(self,t):
+def t_for(t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.key_words.get(t.value,"for")
+        t.type = key_words.get(t.value,"for")
         return t 
     
-    def t_equalequal(self,t):
+def t_equalequal(t):
         r'=='
-        t.type = self.key_words.get(t.value,"equalequal")
+        t.type = key_words.get(t.value,"equalequal")
         return t 
 
 
-    def t_integer(self,t):
+def t_integer(t):
         #rules for lexing integers 
         r'\d+'
-        t.type = self.key_words.get(t.value,"integer")
+        t.type = key_words.get(t.value,"integer")
         return t
     
-    def t_fun(self,t):
+def t_fun(t):
          r'[a-zA-Z_][a-zA-Z_0-9]*'
-         t.type =  self.key_words.get(t.value,"fun")
+         t.type =  key_words.get(t.value,"fun")
          return t
 
-    def t_greaterthanequal(self,t):
+def t_greaterthanequal(t):
         r'>='
-        t.type = self.key_words.get(t.value,"greaterthanequal")
+        t.type = key_words.get(t.value,"greaterthanequal")
         return t
 
 	
-    def t_lessthanequal(self,t):
+def t_lessthanequal(t):
         r'<='
-        t.type = self.key_words.get(t.value,"lessthanequal")
+        t.type = key_words.get(t.value,"lessthanequal")
         return t
     
     
-    def t_equals(self,t):
+def t_equals(t):
         #equals lexer 
         r'=+'
-        t.type = self.key_words.get(t.value,"equals")
+        t.type = key_words.get(t.value,"equals")
         return t
 
-    def t_colon(self,t):
+def t_colon(t):
         r'\;+'
-        t.type = self.key_words.get(t.value,"colon")
+        t.type = key_words.get(t.value,"colon")
         return t
     
-    def t_minus(self,t):
+def t_minus(t):
         r'\-+'
-        t.type = self.key_words.get(t.value,"minus")
+        t.type = key_words.get(t.value,"minus")
         return t 
    
-    def t_times(self,t):
+def t_times(t):
 	r'\*+'
-	t.type =  self.key_words.get(t.value,"times")
+	t.type =  key_words.get(t.value,"times")
 	return t 
 
 
-    def t_whitespace(self,t):
-	r'(\s)'
-        t.type =  self.key_words.get(t.value,"whitespace")
-        return t
-    
-    def t_if(self,t):
+def t_whitespace(t):
+		r'(\s)'
+		t.type =  key_words.get(t.value,"whitespace")
+		return t
+
+def t_if(t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.key_words.get(t.value,'if')
+        t.type = key_words.get(t.value,'if')
         return t
    
-    def t_leftpara(self,t):
+def t_leftpara(t):
 	r'\('
-	t.type = self.key_words.get(t.value,"leftpara")
+	t.type = key_words.get(t.value,"leftpara")
 	return t 
         
-    def t_error(self,t):
+def t_error(t):
 	print("error {}",t.value)
 	t.lexer.skip(1)
 
-    def t_rightpara(self,t):
+def t_rightpara(t):
         r'\)'
-        t.type = self.key_words.get(t.value,"rightpara")
+        t.type = key_words.get(t.value,"rightpara")
         return t
 
-    def t_greaterthan(self,t):
+def t_greaterthan(t):
         r'>'
-        t.type = self.key_words.get(t.value,"greaterthan")
+        t.type = key_words.get(t.value,"greaterthan")
         return t
 
-    def t_rightclosure(self,t):
+def t_rightclosure(t):
         r'{'
-        t.type = self.key_words.get(t.value,"rightclosure")
+        t.type = key_words.get(t.value,"rightclosure")
         return t 
 
-    def t_leftclosure(self,t):
+def t_leftclosure(t):
         r'}'
-        t.type = self.key_words.get(t.value,"leftclosure")
+        t.type = key_words.get(t.value,"leftclosure")
         return t
 
-    def t_lessthan(self,t):
+def t_lessthan(t):
         r'<'
-        t.type = self.key_words.get(t.value,"lessthan")
+        t.type = key_words.get(t.value,"lessthan")
         return t 
 
     
-
-    def t_print(self,t):
+def t_print(t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.key_words.get(t.value,"print")
+        t.type = key_words.get(t.value,"print")
         return t
 
 
-    def build(self,**kwargs):
-         self.lexer = scanner.lex(module=self, **kwargs)
-    
-    def test_function(self,src):
-        #for testing purposes only 
-        self.lexer.input(src)
-        while True:
-            tokens = self.lexer.token()
-            if not tokens:
-                break
-            print(tokens)
-
-
-
+scanner.lex()
--- Parser.py
+++ Parser.py
@@ -1,20 +1,9 @@
-import sys
-#sys.path.insert(0,'../Scanner/')
 import ply.lex as lex
 import ply.yacc as yacc
-#import scanner.Lexer as Lex
-sys.path.insert(0,'../scanner/')
-import scanner.Lexer as Lex
-
+from Lexer import tokens
 
 class Parser():
 
-    def __str__(self):
-        return "parser for the lox programming language"
-
-    def p_indentify(self,p):
-        'id:indetifier'
-        p[0] = p[1]
 
     def build(self):
 		parser = yacc.yacc()
