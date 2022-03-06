768cd89d2f2ea1333151380e8ebaf64ef7b782dc
goodship1 <msgoodship1@gmail.com>
2022-03-06 18:28:34 +0000

    adding more parser.py rules

--- Parser.py
+++ Parser.py
@@ -23,9 +23,73 @@ def p_assignmentofexpression(p):
     '''assignment : VAR identifier equals term plus term colon
                 | VAR identifier equals term minus term colon
                 | VAR identifier equals term times term colon
+                | VAR identifier equals term lessthan term colon
+                | VAR identifier equals term greaterthan term colon
+                | VAR identifier equals term equalequal term colon
+                | VAR identifier equals term lessthanequal term colon
+                | VAR identifier equals term  greaterthanequal term colon
+
                 '''
     p[0] = ("assignment",p[2],p[3],p[4],p[5],p[6])
 
+
+def p_add(p):
+    #non variable addition eg 1+1;
+    'add : term plus term colon'
+    p[0] = ('add',p[1],p[2],p[3])
+
+def p_substract(p):
+    'sub : term miunus term colon'
+    p[0] = ('sub',p[1],p[2],p[3])
+
+def p_mutiply(p):
+    'mutiply : term times term colon'
+    p[0] = ('mutiply',p[1],p[2],p[3])
+
+def p_less(p):
+    'less : term lessthan term colon'
+    p[0] = ('less',p[1],p[2],p[3])
+
+def p_greater(p):
+    'greater : term greaterthan term colon'
+    p[0] = ("greater",p[1],p[2],p[3])
+
+def p_equalequalto(p):
+    'equalcompare : term equalequal term colon'
+    p[0] =("equalcompare",p[1],p[2],p[3])
+
+
+def p_lessequal(p):
+    'lessequal : term lessthanequal term colon'
+    p[0] = ("lessequal",p[1],p[2],p[3])
+
+def p_greaterequal(p):
+    'greaterequal : term greaterthaneqaul term colon'
+    p[0] = ("greaterequal",p[1],p[2],p[3])
+
+
+
+def p_lessthanequal(p):
+    'expression : lessthanequal'
+    p[0] = p[1]
+
+def p_greaterthanequal(p):
+    'expression : greaterthanequal'
+    p[0] = p[1]
+
+
+
+def p_lessthan(p):
+    'expression : lessthan'
+    p[0] = p[1]
+
+def p_greaterthan(p):
+    'expression : greaterthan'
+    p[0] = p[1]
+
+def p_equalequal(p):
+    'expression : equalequal'
+    p[0] = p[1]
 
 
 def p_times(p):
@@ -71,8 +135,8 @@ def p_plus(p):
     'expression : plus'
     p[0] = p[1]
 
-
-parser = yacc.yacc()
+start = 'add'
+parser = yacc.yacc(start = start)
 while True:
     try:
         s = raw_input('Lox > ')
